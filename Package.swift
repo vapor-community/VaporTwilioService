15b53c4887fd534b19b0dc3baceaea4be0ff3f13
Mihael Isaev <isaev.mihael@gmail.com>
2020-01-19 04:23:29 +0400

    Drop platform to `.macOS(.v10_14)`

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Twilio",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
