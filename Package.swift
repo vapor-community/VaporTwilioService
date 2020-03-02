9da5fe1314d75341f56f8669234ecbd688435d2e
Mihael Isaev <isaev.mihael@gmail.com>
2020-03-02 17:56:22 +0400

    Switch to Vapor4 RC

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Twilio",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -14,7 +14,7 @@ let package = Package(
         ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc")
     ],
     targets: [
         .target(name: "Twilio", dependencies: ["Vapor"]),
